b679ed79402e4b18067d5321f44744e29d57a3a0
Nelson Sandoval <nandres.sandoval@gmail.com>
2026-04-01 01:59:01 -0300

    Update admin phone number and whitelist in config

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
     nombreNegocio: "The Best Burger SPA",
     nombreAdmin: "Nelson (Tío Burger)",
-    telefonoAdmin: "56955315829@c.us", // Tu WhatsApp personal
+    telefonoAdmin: "@c.us", // Tu WhatsApp personal
     
     // Telegram para notificaciones
     telegramToken: "", // Pegá tu token completo acá
     telegramChatId: "Id: 6125044592", // Pegá el ID que acabás de obtener (solo números)
     
     modoProduccion: true, // Mantener en false hasta que todo esté testeado
-    whitelistDev: ['56968611939@c.us', '40862755086377@lid'], // Tu WhatsApp para pruebas
+    whitelistDev: ['@c.us', '40862755086377@lid'], // Tu WhatsApp para pruebas
     
     tiempoRespuesta: 1200
 };
